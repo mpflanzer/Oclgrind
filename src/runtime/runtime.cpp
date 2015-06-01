44fdfc179522396f96c3af2f839f6477a6436633
James Price <j.price@bristol.ac.uk>
2015-06-02 00:49:26 +0100

    Added braces around union initializers in cl_image_desc structs.

--- src/runtime/runtime.cpp
+++ src/runtime/runtime.cpp
@@ -1482,7 +1482,7 @@ clCreateImage2D
     0,
     0,
     0,
-    NULL
+    {NULL}
   };
   return clCreateImage(context, flags,
                        image_format, &desc,
@@ -1515,7 +1515,7 @@ clCreateImage3D
     image_slice_pitch,
     0,
     0,
-    NULL
+    {NULL}
   };
   return clCreateImage(context, flags,
                        image_format, &desc,

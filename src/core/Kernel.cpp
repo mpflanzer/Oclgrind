4f593b2a7ab31cff0ca126c92c11251480266151
James Price <j.price@bristol.ac.uk>
2015-06-02 13:29:21 +0100

    Ignore type qualifiers for non-pointer types in clGetKernelArgInfo.

--- src/core/Kernel.cpp
+++ src/core/Kernel.cpp
@@ -310,6 +310,11 @@ unsigned int Kernel::getArgumentTypeQualifier(unsigned int index) const
     return -1;
   }
 
+  // Ignore type qualifiers for non-pointer arguments
+  const llvm::Argument *arg = getArgument(index);
+  if (!arg->getType()->isPointerTy() || arg->hasByValAttr())
+    return CL_KERNEL_ARG_TYPE_NONE;
+
   // Get qualifiers
   llvm::MDString *str =
     llvm::dyn_cast<llvm::MDString>(node->getOperand(index+1));
